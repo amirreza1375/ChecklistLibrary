2093132ff5bac2e9ae524439b2386cff7a5c5649
Amirreza <amirreza@Amirrezas-MacBook-Pro.local>
2021-03-04 16:04:27 +0330

    changed

---
+++ CheckListModule/src/main/java/com/example/checklist/Res.java
@@ -0,0 +1,68 @@
+package com.example.checklist;
+
+public class Res {
+
+    /**
+     * Activities
+     */
+    public static int splash     = R.layout.activity_splash;
+    public static int main       = R.layout.activity_main_new;
+    public static int login      = R.layout.activity_login_new;
+    public static int forgetPass = R.layout.activity_password_new;
+    public static int deleted    = R.layout.activity_deleted_new;
+    public static int shops      = R.layout.activity_shop_new;
+    public static int settings   = R.layout.activity_setting_new;
+    public static int allItems   = R.layout.activity_all_results;
+    public static int checklist  = R.layout.activity_checklist_new;
+    public static int maps       = R.layout.activity_maps;
+    public static int new_new    = R.layout.activity_new_new;
+    public static int scrolling  = R.layout.activity_scrolling;
+    public static int sendTicket = R.layout.activity_send_ticket;
+    public static int tutorial   = R.layout.activity_tutorial;
+    public static int usetTicket = R.layout.activity_user_ticket;
+    public static int webView    = R.layout.activity_web_view;
+
+    /**
+     * Fragments
+     */
+
+    public static int fr_tutorial           = R.layout.fragment_app_tutorial;
+    public static int fr_camera             = R.layout.fragment_camera;
+    public static int fr_checklists         = R.layout.fragment_check_lists;
+    public static int fr_checklist_tutorial = R.layout.fragment_checklist_tutorial;
+    public static int fr_common_issues      = R.layout.fragment_commen_issue;
+    public static int fr_shops              = R.layout.fragment_shops;
+    public static int fr_street             = R.layout.fragment_street;
+    public static int fr_user_issue         = R.layout.fragment_user_issue;
+
+    /**
+     * Layouts
+     */
+    public static int ly_alert_reply_ticket2   = R.layout.layout_alert_reply_ticket2;
+    public static int ly_alert_resend_and_info = R.layout.layout_alert_resend_and_information;
+    public static int ly_calendar              = R.layout.layout_calendar_view;
+    public static int ly_checklist_errors      = R.layout.layout_checklist_errors;
+    public static int ly_choose_app            = R.layout.layout_choose_app;
+    public static int ly_date                  = R.layout.layout_date;
+    public static int ly_deleted_item          = R.layout.layout_deleted_item;
+    public static int ly_get_params_dialog     = R.layout.layout_get_params_dialog;
+    public static int ly_item_checklists       = R.layout.layout_item_check_lists;
+    public static int ly_item_qa               = R.layout.layout_item_qa;
+    public static int ly_item_shop             = R.layout.layout_item_shop;
+    public static int ly_item_shop_new         = R.layout.layout_item_shop_new;
+    public static int ly_location_search       = R.layout.layout_loaction_search;
+    public static int ly_remove_row_alert      = R.layout.layout_remove_row_alert;
+    public static int ly_result_item           = R.layout.layout_result_item;
+    public static int ly_result_item_new       = R.layout.layout_result_item_new;
+    public static int ly_shop_dir_alert        = R.layout.layout_shop_direction_alert;
+    public static int ly_shop_info             = R.layout.layout_shop_info;
+    public static int ly_common_issue_item     = R.layout.layput_common_issue_item;
+    public static int ly_about                 = R.layout.about_layout;
+
+    /**
+     * Others
+     */
+
+    public static int custom_spinner   = R.layout.custom_spinner_item;
+    public static int dialong_download = R.layout.dialog_download_new_app;
+}
